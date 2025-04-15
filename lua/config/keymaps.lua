8ae89b354f1d9b46bc8989135e43241f55801aa7
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-04-15 22:51:51 +0700

    feat(mapping): add initial keymaps
    - Unbind "s" from flash.nvim since it is quite annoying and I prefer Neovim
      built-in "s" keybinding
    - Add <Tab> and friends for buffer cycling rather than "H" and "L" since those
      built-in are awesome
    - Add <C-x> for exiting terminal mode like in NvChad

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local nomap = vim.keymap.del
+
+-- unset
+-- flash.nvim
+nomap("n", "s")
+
+-- bufferline.nvim
+nomap("n", "H")
+nomap("n", "L")
+
+-- set
+map("t", "<C-x>", "<C-\\><C-n>", { desc = "Exit terminal to normal mode" })
+map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Prev buffer" })
+map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+map("n", "<A-i>", "<cmd>terminal<CR>", { desc = "Open terminal" })
